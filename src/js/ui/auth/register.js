29ee5cdddcd9e1c521f189da08122ead26ccf1fd
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2024-09-23 17:44:05 +0200

    Chore: Register page that includes a form that API validates and registers new users. Using import, export, event, target, fetch, response.

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,34 @@
-export async function onRegister(event) {}
+import { API_AUTH_KEY, API_AUTH_REGISTER } from "../../api/constants.js";
+
+export async function onRegister(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    if (!name || !email || !password) {
+        alert("all fields are required!");
+        return;
+    }
+
+    try {
+        const response = await fetch(API_AUTH_REGISTER, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Noroff-API-KEY': API_AUTH_KEY,
+            },
+            body: JSON.stringify ({ name, email, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("registration successful!");
+        } else {
+            alert("registration failed");
+        }
+    } catch (error) {
+        alert("Error. Please try again.")
+    }
+}
