94ae29c22193dadc3404d0af0917ae8f6d9c4a30
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2024-09-25 15:20:33 +0200

    started on create page. not finished

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,23 @@
-export async function createPost({ title, body, tags, media }) {}
+import {API_SOCIAL_POSTS, API_KEY} from "../../api/constants.js";
+
+export async function createPost({ title, body, tags, media }) {
+    const response = await fetch (API_SOCIAL_POSTS, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            title: title, // Required
+            body: body || '', // Optional
+            tags: tags.length > 0 ? tags : [], // Optional, ensure it's an array
+            media: {
+                url: media || '', // Optional, ensure it's a valid URL
+                alt: 'Image description' // Provide a sensible default or gather from user input
+            }
+        })
+    });
+    return response;
+}
+
+
--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,40 @@
-export async function onCreatePost(event) {}
+import { createPost } from '../../api/post/create.js';
+
+export async function onCreatePost(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    const media = event.target.media.value;
+    const tags = Array.from(event.target.querySelectorAll('input[name="tags[]"]')).map(function(input) {
+        return input.value.trim().toLowerCase();
+    }).filter(function(tag) {
+        return tag;
+    });
+    try {
+        const response = await createPost({ title, body, media, tags });
+        if (response.ok) {
+            alert('Your post was created successfully and is now posted!')
+            event.target.reset();
+        } else {
+            alert('Ops! Error. Could not create post.');
+        }
+    } catch (error) {
+        console.error('Error occurred:', error);
+        throw error();
+    }
+}
+
+document.getElementById('create-form').addEventListener('submit', onCreatePost);
+
+const addTagsButton = document.getElementById('add-tag');
+const tagsContainer = document.getElementById('tags-container');
+
+addTagsButton.addEventListener('click', () => {
+    const newTagInput = document.createElement('input');
+    newTagInput.setAttribute('type','text');
+    newTagInput.setAttribute('name','tags[]');
+    newTagInput.setAttribute('placeholder','enter a tag');
+    tagsContainer.appendChild(newTagInput);
+});
+
+
