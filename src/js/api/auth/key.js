ffac7b2f0af8ce93aabab2bea2bfef9ef2224feb
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-09-25 23:06:55 +0200

    chore: added getKey() function. chore: added code to render posts

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,3 +1,11 @@
 export async function getKey(name) {
 
+    const accessToken = localStorage.getItem('accessToken');
+    const username = localStorage.getItem('name');
+
+    if (accessToken && username) {
+        return { accessToken, name: username };
+    } else {
+        throw new Error('AccessToken or username not found');
+    }
 }
