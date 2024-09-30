4e90a0e5ce8ddff9683b9b34aaf73e6245976ad0
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-09-30 22:03:20 +0200

    Feat: Added 'my profile' buttons to all pages. When clicking on it, all data to the logged in user is displayed and the localstorage of another profile is removed. Styling: Added styling to post page and profile page. Feat: Added back buttons to create and edit pages. Fix: On create post, I changed to set the changes to local storage, not session storage.

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -43,7 +43,7 @@ import { API_SOCIAL_POSTS, API_KEY } from "../../api/constants.js"; // Import yo
 export async function createPost({ title, body, media, tags }) {
     try {
         // Retrieve the access token from session storage
-        const accessToken = sessionStorage.getItem('accessToken');
+        const accessToken = localStorage.getItem('accessToken');
 
         console.log("Access Token:", accessToken);
         console.log("API Key:", API_KEY);
